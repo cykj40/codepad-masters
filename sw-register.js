1eded2d983bb236be1963387bb80fd3343c8c040
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-08-12 11:52:34 -0400

    ns

--- sw-register.js
+++ sw-register.js
@@ -1,3 +1,7 @@
-if ("serviceWorker" in navigator) {
-navigator.serviceWorker.register("/sw.js");
-}
+const assets = ["/","styles.css", "app.js", "sw-regisetr.js", "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"];
+
+self.addEventListener('install', event => {
+    caches.open('assets').then(cache => { 
+        cache.addAll(assets);
+    });
+});
